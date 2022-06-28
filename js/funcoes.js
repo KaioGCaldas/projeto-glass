62de0788dd14a6ef332d1a440e0b7cc11aaca045
KaioGCaldas <102546216+KaioGCaldas@users.noreply.github.com>
2022-06-28 19:05:44 -0300

    Finalização da estrutura do formulário

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,3 +1,10 @@
 function mudaFoto(foto) {
     document.getElementById('icone').src = foto;
 }
+
+function calcTotal() {
+    var qtd = parseInt(document.getElementById('cQtd').value);
+    var total = qtd * 1500;
+
+    document.getElementById('cTot').value = total;
+}
